6e4bcf37ce6803a686da6ea170b04f980529e30b
John Maras <sportsballenthusiast@gmail.com>
2022-05-28 13:34:42 -0500

    setup the GET in serverjs

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,8 +1,8 @@
-const pg = require("pg");
+const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-  database: "weekend-to-do-app", // name of database
-  host: "localhost",
+  database: 'weekend-to-do-app', // name of database
+  host: 'localhost',
   port: 5432,
   max: 10, // max number of concurrent connections
   idleTimeoutMillis: 10000, // attempt to connect for 10 seconds
@@ -10,12 +10,12 @@ const config = {
 
 const pool = new Pool(config);
 
-pool.on("connect", () => {
-    console.log("connected to postgres");
+pool.on('connect', () => {
+    console.log('connected to postgres');
 });
 
-pool.on("error", (err) => {
-    console.log("error connecting to postgres", err);
+pool.on('error', (err) => {
+    console.log('error connecting to postgres', err);
 });
 
 
